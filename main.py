6123e0e2ba09e9436937f4a6214e2f070381cb37
GalenHare <galenhare@hotmail.com>
2020-04-02 07:38:17 -0500

    Marking attendance working

--- main.py
+++ main.py
@@ -4,29 +4,35 @@ import time
 import datetime
 from pyfingerprint.pyfingerprint import PyFingerprint
 
-url = "http://192.168.0.3:5000/"
+url = "http://192.168.0.9:5000/"
 
+scannerID = 1
+endDay = -1
+endHour = -1
+endMinute = -1
+courseCode = 0
+startMinute = -1
 ## Main program
 def main():
     initializeSensor()
     while(True):
-        try:
-            print("Welcome to the the Registration Authentiction Unit")
-            selection = None
-            while(selection != "1" and selection != "2"):
-                print("Please select an option from below")
-                print("\t1:Enroll a fingerprint\n\t2:Mark Attendance")
-                ##TODO: Ensure admin user before enrolling a fingerprint
-                selection = input()
-                if(selection != "1" and selection != "2"):
-                    print("Invalid selection please try again")
-            if(selection == "1"):
-                enrollFingerprint()
-            elif(selection == "2"):
-                markAttendance()
-        except Exception as e:
-            print('Operation failed!')
-            print('Exception message: ' + str(e))
+        ###try:
+        print("Welcome to the the Registration Authentiction Unit")
+        selection = None
+        while(selection != "1" and selection != "2"):
+            print("Please select an option from below")
+            print("\t1:Enroll a fingerprint\n\t2:Mark Attendance")
+            ##TODO: Ensure admin user before enrolling a fingerprint
+            selection = input()
+            if(selection != "1" and selection != "2"):
+                print("Invalid selection please try again")
+        if(selection == "1"):
+            enrollFingerprint()
+        elif(selection == "2"):
+            markAttendance()
+#except Exception as e:
+ #           print('Operation failed!')
+  #          print('Exception message: ' + str(e))
            # exit(1)
 
 def initializeSensor():
@@ -123,25 +129,28 @@ def enrollFingerprint():
     print("Success")
 
 def attendanceRequest(id):
+    global endMinute
     #TODO: Add exception handling for this function only
     global currentDateTime
     currentDateTime = datetime.datetime.now()
-    print("HERE")
+    print(endMinute)
     if(timeParser('currentTimeMins')>int(endMinute) or timeParser('currentDay')!=int(endDay)):
         courseCode,endDay,startMinute,endMinute = requestCourse()
-    payload = {'studentID':id,"date":timeParser('JSFormat'),'courseCode':courseCode} 
+    payload = {'studentID':id,"date":timeParser('JSFormat'),'courseCode':courseCode}
+    print(payload)
     r = requests.post(url+'api/attendance',json = payload)
     r.raise_for_status()
     print(r.text)
 
 def requestCourse():
-    payload = {"_id":scannerID,"currentDate":'2020-03-25T14:00:00'}
+    payload = {"_id":scannerID,"currentDate":"2020-03-25T14:00:00"}
     """timeParser("JSFormat")"""
-    r = requests.get(url+'/api/scanner/course',params = payload)
+    r = requests.get(url+'api/scanner/course',params = payload)
     r.raise_for_status()
     response = r.json()
+    print(response)
     #TODO: add contingency for nothing returned as well as for multiple
-    return response['courseCode'],response['day'],response['startMinute'],response['endMinute']
+    return response[0]['courseCode'],response[0]['day'],response[0]['startMinute'],response[0]['endMinute']
     
     
 
@@ -151,19 +160,7 @@ def timeParser(option):
     elif(option == 'currentDay'):
         return int(currentDateTime.strftime("%w"))
     elif(option == 'JSFormat'):
-        currentDateTime.strftime("%Y-%m-%dT%X")
+        return currentDateTime.strftime("%Y-%m-%dT%X")
+        
 if __name__ == '__main__':
-    global scannerID
-    scannerID = 1
-    global endDay
-    endDay = -1
-    global endHour
-    endHour = -1
-    global endMinute
-    endMinute = -1
-    global courseCode
-    courseCode = 0
-    global startMinute
-    startMinute = -1
-    print(endMinute)
     main()
